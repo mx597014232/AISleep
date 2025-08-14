a2b94ebaedc3d96df7397eb574795feccc762c1d
mx597014232 <597014232@qq.com>
2025-08-14 16:59:55 +0800

    Update example_usage.py

--- aisleep_code/example_usage.py
+++ aisleep_code/example_usage.py
@@ -11,10 +11,19 @@ UK-Sleep 重构模块使用示例
 2. 再运行 plotting_functions.py 进行可视化
 3. 本文件提供完整的流程示例
 """
-from aisleep_code.plotting_functions import fig_2a_plot_gamma_hist
+from aisleep_code.plotting_functions import (fig_2a_plot_gamma_hist, fig_2_b_plot_wake, fig_2_c_boxplot,
+                                             fig_3_plot_irasa_new, fig_3_e_spindle_boxplot, fig_4_plot_n3,
+                                             fig_4_so_boxplot, fig_5d_osc_boxplot, fig_5_plot_alpha)
 
 def main():
-    fig_2a_plot_gamma_hist()
+    # fig_2a_plot_gamma_hist()
+    # fig_2_b_plot_wake()
+    # fig_2_c_boxplot()
+    # fig_3_plot_irasa_new()
+    # fig_4_plot_n3()
+    # fig_4_so_boxplot()
+    fig_5_plot_alpha()
+
 
 if __name__ == "__main__":
     main()
